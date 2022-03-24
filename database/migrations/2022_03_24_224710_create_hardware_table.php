8ce5f438170e74c9286413c436b7ed0d53cf2c98
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-24 16:00:20 -0700

    Create 2022_03_24_224710_create_hardware_table.php

---
+++ database/migrations/2022_03_24_224710_create_hardware_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHardwareTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hardware', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->string('os');
+            $table->string('cpu');
+            $table->string('gpu');
+            $table->string('storage');
+            $table->string('ram');
+            $table->string('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hardware');
+    }
+}
