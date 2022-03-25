bbc181075220375d15a286754a90810aae34100a
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-25 13:53:04 -0700

    Update edit.blade.php

--- resources/views/manufacturers/edit.blade.php
+++ resources/views/manufacturers/edit.blade.php
@@ -11,11 +11,11 @@
     <div class="col-md-12">
         <div class="card card-primary">
             <div class="card-body">              
-                <form method="post" action="{{ route('manufacturers.update', ['manufacturers'=>$manufacturers->id]) }}" >
+                <form method="post" action="{{ route('manufacturers.update', ['manufacturer'=>$manufacturers->id]) }}" >
     @csrf
     <input type="hidden" name="_method" value="PUT">
         <div class="row">
-        <x-adminlte-select name="type" label="Type" fgroup-class="col-md-6">
+        <x-adminlte-input name="name" label="Name" fgroup-class="col-md-6" />
         <x-adminlte-input name="sales" label="Sales Contact" fgroup-class="col-md-6"  />
         <x-adminlte-input name="tech" label="Tech Support" fgroup-class="col-md-6"  />
         <x-adminlte-button type="Submit" label="Submit" />
