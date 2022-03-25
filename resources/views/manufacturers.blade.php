ba6fbceaa59072cd0d09bbbcbc47d51116338c12
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-25 13:42:00 -0700

    Update manufacturers.blade.php

--- resources/views/manufacturers.blade.php
+++ resources/views/manufacturers.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="card-body">
                 @foreach($manufacturers AS $manufacturer)
-                <li><a href="{{ route('manufacturers.show', ['manufacturers'=>$manufacturer->id]) }}">{{ $manufacturers->name }}</a></li>
+                <li><a href="{{ route('manufacturers.show', ['manufacturer'=>$manufacturers->id]) }}">{{ $manufacturers->name }}</a></li>
                 @endforeach
             </div>
             <div class="card-footer">
