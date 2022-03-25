702b04c20995285e36d2111146cbc54565f4b392
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-25 00:53:35 -0700

    Update hardware.blade.php

--- resources/views/hardware.blade.php
+++ resources/views/hardware.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="card-body">
                 @foreach($hardware AS $hardware)
-                <li><a href="{{ route('hardware.show', ['hardware'=>$hardware->id]) }}">{{ $hardware=>name }}</a></li>
+                <li><a href="{{ route('hardware.show', ['hardware'=>$hardware->id]) }}">{{ $hardware->name }}</a></li>
                 @endforeach
             </div>
             <div class="card-footer">
