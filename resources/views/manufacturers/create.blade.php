9ae9a296b945332a8bbf1f93a0e1798e7ef3b996
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-26 13:58:55 -0700

    z

--- resources/views/manufacturers/create.blade.php
+++ resources/views/manufacturers/create.blade.php
@@ -18,9 +18,9 @@
         <x-adminlte-input name="name" label="Name" fgroup-class="col-md-6" />
         <x-adminlte-input name="sales" label="Sales Contact" fgroup-class="col-md-6"  />
         <x-adminlte-input name="tech" label="Tech Support" fgroup-class="col-md-6"  />
-        <x-adminlte-select name="hardware_name" label="Hardware" fgroup-class="col-md-6" >    
+        <x-adminlte-select name="hardware_id" label="Hardware" fgroup-class="col-md-6" multiple>    
         @foreach($hardware AS $hardware)
-        <option value="{{ $hardware->name }}" > {{ $hardware->name }}</option>
+        <option value="{{ $hardware->id }}" > {{ $hardware->name }}</option>
         @endforeach
         </x-adminlte-select>
         <x-adminlte-button type="Submit" label="Submit" />
