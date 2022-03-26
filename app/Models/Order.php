1072284cb147870376c53b8d03ac9bacd390f37d
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-25 17:33:25 -0700

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    protected $fillable = ['invoice','date','notes,'];
+    protected $fillable = ['invoice','date','notes',];
     public $timestamps = false;
 }
