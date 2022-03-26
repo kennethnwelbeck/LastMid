b060f76c67207b790e11e1d7ff77fab14c981f00
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-26 16:15:04 -0700

    s

--- resources/views/hardware/edit.blade.php
+++ resources/views/hardware/edit.blade.php
@@ -24,29 +24,29 @@
         </x-adminlte-select> 
 
         <x-adminlte-select name="os" label="OS" value="{{ old('os', $hardware->os) }}" fgroup-class="col-md-6">
-        <option>Windows</option>
-        <option>MacOS</option>
-        <option>Linux</option>
-        <option>Android</option>
-        <option>iOS</option>
+        <option  {{ $hardware->os == "Windows" ? "selected" : "" }}>Windows</option>
+        <option  {{ $hardware->os == "MacOS" ? "selected" : "" }}>MacOS</option>
+        <option  {{ $hardware->os == "Linux" ? "selected" : "" }}>Linux</option>
+        <option {{ $hardware->os == "Android" ? "selected" : "" }}>Android</option>
+        <option  {{ $hardware->os == "iOS" ? "selected" : "" }}>iOS</option>
         </x-adminlte-select>
 
         <x-adminlte-input name="cpu" label="CPU" value="{{ old('cpu', $hardware->cpu) }}" fgroup-class="col-md-6"  />
         <x-adminlte-input name="gpu" label="GPU" value="{{ old('gpu', $hardware->gpu) }}" fgroup-class="col-md-6"  />
     
         <x-adminlte-select name="storage" label="Storage" value="{{ old('storage', $hardware->storage) }}" fgroup-class="col-md-6">
-        <option>60GB</option>
-        <option>120GB</option>       
-        <option>250GB</option>
-        <option>500GB</option>
-        <option>1TB</option>
-        <option>2TB</option>
+        <option  {{ $hardware->storage == "60GB" ? "selected" : "" }}>60GB</option>
+        <option {{ $hardware->storage == "120GB" ? "selected" : "" }}>120GB</option>       
+        <option {{ $hardware->storage == "250GB" ? "selected" : "" }}>250GB</option>
+        <option {{ $hardware->storage == "500GB" ? "selected" : "" }}>500GB</option>
+        <option {{ $hardware->storage == "1TB" ? "selected" : "" }}>1TB</option>
+        <option {{ $hardware->storage == "2TB" ? "selected" : "" }}>2TB</option>
         </x-adminlte-select>
 
         <x-adminlte-select name="ram" label="RAM" value="{{ old('ram', $hardware->ram) }}" fgroup-class="col-md-6">
-        <option>4GB</option>
-        <option>8GB</option>
-        <option>16GB</option>
+        <option {{ $hardware->ram == "4GB" ? "selected" : "" }}>4GB</option>
+        <option {{ $hardware->storage == "8GB" ? "selected" : "" }}>8GB</option>
+        <option {{ $hardware->storage == "16GB" ? "selected" : "" }}>16GB</option>
         </x-adminlte-select>
 
         <x-adminlte-input name="price" label="Price" value="{{ old('price', $hardware->price) }}" fgroup-class="col-md-6"  />
