6c8de2cdfe7daec705dc191a932b3498ea33f885
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-26 20:24:40 -0700

    Update 2022_03_25_225616_create_buyers_table.php

--- database/migrations/2022_03_25_225616_create_buyers_table.php
+++ database/migrations/2022_03_25_225616_create_buyers_table.php
@@ -14,7 +14,7 @@ class CreateBuyersTable extends Migration
             $table->string('last');
             $table->string('email');
             $table->string('phone');
-            $table->foreignId('order_id')->nullable()->constrained('order')->defualt(NULL);
+         
             $table->foreignId('hardware_id')->nullable()->constrained('hardware')->defualt(NULL);
             $table->timestamps();
         });
