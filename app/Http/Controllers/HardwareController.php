6e287c585828e4f9dfe2cfee7a7438b70695dfb3
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-27 00:12:11 -0700

    n

--- app/Http/Controllers/HardwareController.php
+++ app/Http/Controllers/HardwareController.php
@@ -15,10 +15,17 @@ class HardwareController extends Controller
         return view('hardware', compact('hardware'));
     }
 
+    public function desktops()
+    {
+        $hardware = Hardware::all();
+        return view('hardware.desktops', compact('hardware'));
+    }
+
     public function create()
     {
         return view('hardware.create');
     }
+
 
     public function store(Request $request)
     {
