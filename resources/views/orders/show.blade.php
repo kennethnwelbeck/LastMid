6abff139c0f316fbd7a65cb431bfa4e0cc291787
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-26 22:00:22 -0700

    s

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -30,7 +30,8 @@
                 @if($orders->hardware_id != NULL)
                 @foreach($hardware AS $hardware)
                 @if($hardware->id == $orders->hardware_id)
-                <b>Purchased Hardware:</b><a href="{{ route('hardware.show', ['hardware'=>$orders->hardware_id]) }}">{{ $hardware->id.' '.$hardware->name'}}</a> | <b>Price:</b>{{ $hardware->price }}
+                <b>Purchased Hardware:</b><a href="{{ route('hardware.show', ['hardware'=>$orders->hardware_id]) }}">{{ $hardware->id.' '.$hardware->name}}</a> 
+                <b>Price:</b>{{ $hardware->price }}
                 @endif
                 @endforeach
                 @endif 
