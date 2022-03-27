ab5e73c153e1858db565de7631ddb8d6897e3d82
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-26 21:15:31 -0700

    Update show.blade.php

--- resources/views/manufacturers/show.blade.php
+++ resources/views/manufacturers/show.blade.php
@@ -21,7 +21,7 @@
                 <b>Sales:</b> {{ $manufacturers->sales }} |
                 <b>Tech Support:</b> {{ $manufacturers->tech }} |
                 @if($manufacturers->hardware_id != NULL)
-                @foreach(manufacturer->hardware AS $hardware)
+                @foreach($hardware AS $hardware)
                 @if($hardware->id == $manufacturers->hardware_id)
                 <b>Hardware:</b><a href="{{ route('hardware.show', ['hardware'=>$manufacturers->hardware_id]) }}">{{ $hardware->id.' '.$hardware->name }}</a>
                 @endif
