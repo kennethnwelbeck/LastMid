0be5f44eca342b1b7df8fcdfa3554fe7a2e85365
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-26 23:15:13 -0700

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -227,98 +227,53 @@ return [
     'menu' => [
         // Navbar items:
         [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
+            'text' => 'Hardware',
+            'url'  => '#',
+        ], 
         [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],
-
-        // Sidebar items:
-        [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
-        [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
-        ],
-        [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
+            'text'    => 'View Hardware By',
             'submenu' => [
                 [
-                    'text' => 'level_one',
+                    'text' => 'Category',
                     'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
+                    'submenu' =>[
                         [
-                            'text' => 'level_two',
+                            'text' => 'Computers',
                             'url'  => '#',
                         ],
                         [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
+                            'text' => 'Laptops',
+                            'url'  => '#',
                         ],
-                    ],
+                        [
+                            'text' => 'Tablets',
+                            'url'  => '#',
+                        ],
+                    ]
                 ],
                 [
-                    'text' => 'level_one',
+                    'text' => 'Manufacturer',
+                    'url'  => '#',
+                ],
+                [
+                    'text' => 'User',
                     'url'  => '#',
                 ],
             ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
+            
         ],
         [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
+            'text' => 'Orders',
+            'url'  => '#',
         ],
         [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
+            'text' => 'Manufacturers',
+            'url'  => '#',
         ],
-    ],
+        [
+            'text' => 'Users',
+            'url'  => '#',
+        ],
 
     /*
     |--------------------------------------------------------------------------
