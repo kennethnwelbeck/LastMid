a2adb6565d301d536470f19d2865c3cce54e7347
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-27 00:10:28 -0700

    Update desktops.blade.php

---
+++ resources/views/hardware/desktops.blade.php
@@ -0,0 +1,36 @@
+@extends('adminlte::page')
+
+@section('title', 'Hardware')
+
+@section('content_header')
+    <h1>Hardware</h1>
+@stop
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card card-default">
+            <div class="card-header">
+                <a href="{{ route('hardware.create') }}" class="btn btn-primary float-right">Add Hardware</a>
+            </div>
+            <div class="card-body">
+                @foreach($hardware AS $hardware)
+                <li><a href="{{ route('hardware.show', ['hardware'=>$hardware->id]) }}">{{ $hardware->name }}</a></li>
+                @endforeach
+
+    
+            </div>
+            <div class="card-footer">
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
