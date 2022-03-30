a733dbe3f689f4d3816c6818c44f3777686561da
KennethWelbeck <007357578@coyote.csusb.edu>
2022-03-29 21:55:09 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,12 +11,7 @@
     <div class="col-md-12">
         <div class="card card-default">
             <div class="card-header">
-                <a href="{{ route('hardware.create') }}" class="btn btn-primary float-right">Add Hardware</a>
-            </div>
-            <div class="card-body">
-                @foreach($hardware AS $hardware)
-                <li><a href="{{ route('hardware.show', ['hardware'=>$hardware->id]) }}">{{ $hardware->name }}</a></li>
-                @endforeach
+    
 
     
             </div>
